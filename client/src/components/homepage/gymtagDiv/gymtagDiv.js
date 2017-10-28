44a5d5e58da523419c09611fb59483a9fb995697
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-28 17:53:15 -0400

    description change

--- client/src/components/homepage/gymtagDiv/gymtagDiv.js
+++ client/src/components/homepage/gymtagDiv/gymtagDiv.js
@@ -20,7 +20,7 @@ const gymtagDiv = () => (
       <div className="paragraph">
         <h3>Gym-Buddy</h3>
             <h4></h4>
-              <p className="gym_description">A website that connects gym members who are looking for a workout partner that has some knowledge of proper lifting techniques, but are not considered a certified trainer. This was achieved through the use of MySQL to link together tables of data to provide scheduling and account signup. The design aspect utilized Bootstrap to provide the ascetics and node.js to build the server and corresponding routes.</p>
+              <p className="gym_description">Connects gym members who are looking for a workout partner that has some knowledge of proper lifting techniques, but are not considered a certified trainer. This was achieved through the use of MySQL to link together tables of data to provide scheduling and account signup. The design aspect utilized Bootstrap to provide the ascetics and node.js to build the server and corresponding routes.</p>
               <a href="https://gympursuit.herokuapp.com/">
                 <button className="btn btn-primary">Home Page</button>
               </a>
