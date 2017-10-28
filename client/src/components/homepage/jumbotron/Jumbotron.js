3f32048ae80d3995bd7dad90bb79991d553e2c32
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-28 17:12:31 -0400

    website spacing

--- client/src/components/homepage/jumbotron/Jumbotron.js
+++ client/src/components/homepage/jumbotron/Jumbotron.js
@@ -16,9 +16,6 @@ const Jumbotron = () => (
             <h1 className="text-center Name"> Joseph Calabrese </h1>
               <h2 className="text-center Job">Full Stack Web Developer</h2>
               <br></br><br></br>
-                <div className="col-sm-6 col-md-6 col-lg-6 photo">     
-                    <img className="mainphoto" src="./images/joe1.JPG" alt="priceImage" />              
-                </div>
         </div>
 
     </div>    
