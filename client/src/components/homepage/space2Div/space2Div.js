1bcc229c2abd347d3c32460cbfcf0d20030610e5
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-28 17:42:16 -0400

    picture sizes

--- client/src/components/homepage/space2Div/space2Div.js
+++ client/src/components/homepage/space2Div/space2Div.js
@@ -13,7 +13,7 @@ const space2Div = () => (
 
       <div className="col-sm-6 col-md-6 col-lg-6">
             <img className="about" src="./images/gears.png" alt="About Me" />
-             <p className="text-center space1description textborder">
+             <p className="text-center space2description textborder">
               Multiple years of management experience that can be applied to any field with aspirations to become a Project Manager. Highly motivated goal oriented professional; a decision-maker skilled in implementing new ideas.
              </p>
       </div>
--- client/src/components/homepage/vodlegendsDiv/vodlegendsDiv.js
+++ client/src/components/homepage/vodlegendsDiv/vodlegendsDiv.js
@@ -14,8 +14,9 @@ const vodlegendsDiv = () => (
     <div className="row">
       <div className="col-sm-6 col-md-6 col-lg-6"> 
       <div className="paragraph">
-        <img className="vodlegendstitle" src="./images/vodlg.png" alt="priceImage" />
-
+          <a href="https://vod-legends.herokuapp.com/">
+            <img className="vodlegendstitle" src="./images/vodlg.png" alt="priceImage" />
+          </a>
             <p className="description">An E-Sports connection hub that was designed for the PC game League of Legends and built using React. Where professional players can provide live feedback through video playback interface for players looking to increase their ELO ranking. Using MongoDB to store all relative information for user profiles, requests, and comments.</p>
       </div>
       </div>
