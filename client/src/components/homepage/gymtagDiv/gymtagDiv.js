2b7ab2c37d2a950fece998dd88a3e6dbeb461fa3
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-30 19:14:36 -0400

    update gym

--- client/src/components/homepage/gymtagDiv/gymtagDiv.js
+++ client/src/components/homepage/gymtagDiv/gymtagDiv.js
@@ -12,18 +12,19 @@ const gymtagDiv = () => (
     </div>
 
     <div className="row">
-      <div className="col-sm-6 col-md-6 col-lg-6">        
-        <img className="gym" src="./images/GymBuddy.png" alt="GymBuddy" />
+      <div className="col-sm-6 col-md-6 col-lg-6">
+        <a href="https://gympursuit.herokuapp.com/">    
+          <img className="gym" src="./images/GymBuddy.png" alt="GymBuddy" />
+        </a>
       </div>
 
       <div className="col-sm-6 col-md-6 col-lg-6">
       <div className="paragraph">
-        <h3>Gym-Buddy</h3>
-            <h4></h4>
-              <p className="gym_description">Connects gym members who are looking for a workout partner that has some knowledge of proper lifting techniques, but are not considered a certified trainer. This was achieved through the use of MySQL to link together tables of data to provide scheduling and account signup. The design aspect utilized Bootstrap to provide the ascetics and node.js to build the server and corresponding routes.</p>
-              <a href="https://gympursuit.herokuapp.com/">
-                <button className="btn btn-primary">Home Page</button>
-              </a>
+        <a href="https://gympursuit.herokuapp.com/">    
+          <img className="gymtitle" src="./images/gym.png" alt="GymBuddy" />
+        </a>
+            <br></br><br></br>
+              <p className="gym_description">Connects gym members who are looking for a workout partner that has some knowledge of proper lifting techniques, but are not considered a certified trainer. This was achieved through the use of MySQL to link together tables of data to provide scheduling and account signup. The design aspect utilized Bootstrap to provide the ascetics and node.js to build the server and corresponding routes.</p>  
       </div>
       </div>
     </div>    
